b662cdbe4d27adadaac7bb794e869d31f827452b
titanism <101466223+titanism@users.noreply.github.com>
2022-06-13 18:19:32 -0500

    chore: fixed redis test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 const I18N = require('@ladjs/i18n');
+const Redis = require('ioredis-mock');
 const del = require('del');
-const test = require('ava');
 const delay = require('delay');
+const test = require('ava');
 
 const Mandarin = require('..');
 
@@ -12,7 +13,7 @@ test('translates a basic phrase to multiple languages', async (t) => {
   const i18n = new I18N({ autoReload: true });
   const hello = i18n.api.t({ phrase: 'Hello', locale: 'en' });
   t.is(hello, 'Hello');
-  const mandarin = new Mandarin({ i18n });
+  const mandarin = new Mandarin({ i18n, redis: new Redis() });
   await mandarin.translate();
   // allow auto-reload to occur
   await delay(1000);
